30e940e864bb09b6e4d1ecc8dec00fcf24b97839
BERKABZ <150333064+BERKABZ@users.noreply.github.com>
2023-11-22 12:28:14 -0500

    Add files via upload

---
+++ JEANNEE/src/main/java/com/mycompany/jeannee/JEANNEE.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.jeannee;
+
+/**
+ *
+ * @author Edward
+ */
+public class JEANNEE {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
